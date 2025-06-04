30adf0d488e1275c7a7cfe039c8d241c530e8b67
Josip <josipkeresman@gmail.com>
2025-06-05 01:15:05 +0200

    Refactor bunch of nonsense

---
+++ IGNGames/src/main/java/com/keresman/view/designer/ProfilePanelDesigner.java
@@ -0,0 +1,177 @@
+package com.keresman.view.designer;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+public abstract class ProfilePanelDesigner extends JPanel {
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    protected javax.swing.JButton btnUpdateProfile;
+    protected javax.swing.JTextField lblEditProfile;
+    protected javax.swing.JLabel lblErrorEmail;
+    protected javax.swing.JLabel lblErrorFirstName;
+    protected javax.swing.JLabel lblErrorLastName;
+    protected javax.swing.JLabel lblErrorUsername;
+    protected javax.swing.JLabel lblProfileImage;
+    protected javax.swing.JLabel lblmage;
+    protected javax.swing.JPanel pnlContainer;
+    protected javax.swing.JTextField tfEmail;
+    protected javax.swing.JTextField tfFirstName;
+    protected javax.swing.JTextField tfLastName;
+    protected javax.swing.JTextField tfUsername;
+    // End of variables declaration//GEN-END:variables
+
+    protected ProfilePanelDesigner() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblmage = new javax.swing.JLabel();
+        pnlContainer = new javax.swing.JPanel();
+        lblEditProfile = new javax.swing.JTextField();
+        btnUpdateProfile = new javax.swing.JButton();
+        tfUsername = new javax.swing.JTextField();
+        tfFirstName = new javax.swing.JTextField();
+        tfLastName = new javax.swing.JTextField();
+        tfEmail = new javax.swing.JTextField();
+        lblProfileImage = new javax.swing.JLabel();
+        lblErrorFirstName = new javax.swing.JLabel();
+        lblErrorLastName = new javax.swing.JLabel();
+        lblErrorUsername = new javax.swing.JLabel();
+        lblErrorEmail = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(36, 36, 36));
+        setLayout(new java.awt.BorderLayout());
+
+        lblmage.setBackground(new java.awt.Color(24, 24, 24));
+        lblmage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/profile12.jpg"))); // NOI18N
+        lblmage.setPreferredSize(new java.awt.Dimension(400, 800));
+        add(lblmage, java.awt.BorderLayout.LINE_END);
+
+        pnlContainer.setBackground(new java.awt.Color(24, 24, 24));
+        pnlContainer.setPreferredSize(new java.awt.Dimension(800, 800));
+
+        lblEditProfile.setBackground(new java.awt.Color(24, 24, 24));
+        lblEditProfile.setFont(new java.awt.Font("SansSerif", 1, 16)); // NOI18N
+        lblEditProfile.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        lblEditProfile.setText("Edit profile");
+        lblEditProfile.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 20, 10, 0));
+
+        btnUpdateProfile.setBackground(new java.awt.Color(102, 102, 255));
+        btnUpdateProfile.setFont(new java.awt.Font("SansSerif", 1, 16)); // NOI18N
+        btnUpdateProfile.setForeground(new java.awt.Color(255, 255, 255));
+        btnUpdateProfile.setText("Save changes");
+        btnUpdateProfile.setBorder(null);
+        btnUpdateProfile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateProfileActionPerformed(evt);
+            }
+        });
+
+        tfUsername.setBackground(new java.awt.Color(24, 24, 24));
+        tfUsername.setFont(new java.awt.Font("Segoe UI Black", 0, 12)); // NOI18N
+        tfUsername.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Username", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+        tfUsername.setPreferredSize(new java.awt.Dimension(65, 30));
+
+        tfFirstName.setBackground(new java.awt.Color(24, 24, 24));
+        tfFirstName.setFont(new java.awt.Font("Segoe UI Black", 0, 12)); // NOI18N
+        tfFirstName.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "First name", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+
+        tfLastName.setBackground(new java.awt.Color(24, 24, 24));
+        tfLastName.setFont(new java.awt.Font("Segoe UI Black", 0, 12)); // NOI18N
+        tfLastName.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Last name", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+
+        tfEmail.setBackground(new java.awt.Color(24, 24, 24));
+        tfEmail.setFont(new java.awt.Font("Segoe UI Black", 0, 12)); // NOI18N
+        tfEmail.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Email", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
+
+        lblProfileImage.setBackground(new java.awt.Color(153, 153, 153));
+        lblProfileImage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblProfileImage.setText("Profle image");
+        lblProfileImage.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+
+        lblErrorFirstName.setForeground(new java.awt.Color(255, 51, 51));
+        lblErrorFirstName.setText("X");
+
+        lblErrorLastName.setForeground(new java.awt.Color(255, 51, 51));
+        lblErrorLastName.setText("X");
+
+        lblErrorUsername.setForeground(new java.awt.Color(255, 51, 51));
+        lblErrorUsername.setText("X");
+
+        lblErrorEmail.setForeground(new java.awt.Color(255, 51, 51));
+        lblErrorEmail.setText("X");
+
+        javax.swing.GroupLayout pnlContainerLayout = new javax.swing.GroupLayout(pnlContainer);
+        pnlContainer.setLayout(pnlContainerLayout);
+        pnlContainerLayout.setHorizontalGroup(
+            pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlContainerLayout.createSequentialGroup()
+                .addComponent(lblEditProfile)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlContainerLayout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addComponent(lblProfileImage, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(192, 192, 192)
+                .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(pnlContainerLayout.createSequentialGroup()
+                        .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(tfEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tfUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblErrorUsername)
+                            .addComponent(lblErrorEmail)
+                            .addComponent(lblErrorLastName)
+                            .addComponent(lblErrorFirstName)))
+                    .addComponent(tfLastName, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfFirstName, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(407, Short.MAX_VALUE))
+            .addGroup(pnlContainerLayout.createSequentialGroup()
+                .addGap(442, 442, 442)
+                .addComponent(btnUpdateProfile, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        pnlContainerLayout.setVerticalGroup(
+            pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlContainerLayout.createSequentialGroup()
+                .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(pnlContainerLayout.createSequentialGroup()
+                        .addGap(104, 104, 104)
+                        .addComponent(lblEditProfile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(pnlContainerLayout.createSequentialGroup()
+                                .addGap(218, 218, 218)
+                                .addComponent(lblErrorFirstName))
+                            .addGroup(pnlContainerLayout.createSequentialGroup()
+                                .addGap(35, 35, 35)
+                                .addComponent(tfFirstName, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(pnlContainerLayout.createSequentialGroup()
+                        .addGap(190, 190, 190)
+                        .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(tfLastName, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblProfileImage, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(18, 18, 18)
+                .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblErrorLastName)
+                    .addComponent(tfUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(47, 47, 47)
+                .addComponent(lblErrorUsername)
+                .addGap(18, 18, 18)
+                .addGroup(pnlContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tfEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblErrorEmail))
+                .addGap(35, 35, 35)
+                .addComponent(btnUpdateProfile, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(118, Short.MAX_VALUE))
+        );
+
+        add(pnlContainer, java.awt.BorderLayout.CENTER);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public abstract void btnUpdateProfileActionPerformed(ActionEvent evt);
+
+}
